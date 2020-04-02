3d42f6b5755e0dcb97c3fe5b78f5d8cb40e279ce
Chris Harvey <chris-harvey94@gmail.com>
2020-04-02 12:24:37 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
+window.addEventListener('DOMContentLoaded', () => {
+	
 
+// UI Vars 
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+const navSlide = () => {
 	burger.addEventListener('click', () => {
 		//Toggle Nav
 		nav.classList.toggle('nav-active');
-
+		
 		//Animate Links
 		navLinks.forEach((link, index) => {
 			if (link.style.animation) {
 				link.style.animation = '';
 			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
 			}
         });
         //Burger Animation
         burger.classList.toggle('toggle');
 	});
-}
-
+};
 	navSlide();
+	// smooth scroll 
+	const scroll = new SmoothScroll('a[href*="#"]', {
+		speed: 500
+
+	});
+		
+	const navToSolid = () => {
+		const navSolid = document.getElementById('nav');
+		window.onscroll = () => {
+			if(window.pageYOffset > 200) {
+				navSolid.style.background = "#333";
+			} else {
+				navSolid.style.background = "transparent";
+			}
+		}
+	}
+
+	navToSolid();
+	// Typewriter effect
+		
+	// Contact form
+
+});
